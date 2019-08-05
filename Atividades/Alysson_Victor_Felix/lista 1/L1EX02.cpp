eb9281275177cc0786e33d8c62b4451929044f20
alyssonfelix2000 <53452728+alyssonfelix2000@users.noreply.github.com>
2019-08-05 20:00:04 -0300

    Add files via upload

---
+++ Atividades/Alysson_Victor_Felix/lista 1/L1EX02.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:     Alysson Victor
+- Descrição: Comando endl - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << " Aula de automação!" << endl;
+
+    return 0;
+}
---
+++ Atividades/Alysson_Victor_Felix/lista 1/L1EX05.cpp
@@ -0,0 +1,33 @@
+/**********************************************************
+- Autor:     Alysson Victor
+- Descrição: Comando endl - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "        *        " << endl;
+    cout << "       * *      " << endl;
+    cout << "      *   *      " << endl;
+    cout << "     *     *     " << endl;
+    cout << "    *       *    " << endl;
+    cout << "   *         *   " << endl;
+    cout << "  *************  " << endl;
+    cout << "       ***       " << endl;
+    cout << "       ***       " << endl;
+    cout << "       ***       " << endl;
+
+
+
+
+    return 0;
+}
