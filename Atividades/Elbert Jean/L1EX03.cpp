1ff7f1a517d6121e0ba42022a4835a85df6ba165
ElbertJean <53452699+ElbertJean@users.noreply.github.com>
2019-08-05 13:57:17 -0300

    Add files via upload

---
+++ Atividades/Elbert Jean/L1EX03.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:     Elbert Jean
+- Descrição: Comando \n - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Hoje é aula de Programação!\n";
+
+    return 0;
+}
