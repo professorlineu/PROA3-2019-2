3af585ff7a3a8ea12775f1dfd5e442ed59d76262
Cauebiel18 <53452755+Cauebiel18@users.noreply.github.com>
2019-08-05 19:50:54 -0300

    Add files via upload

---
+++ Atividades/Caue_Gabriel/lista 1/L1EX09_Caue.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+- Autor:     Cauê Gabriel
+- Descrição: Nome inteiro ex.10
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "\tCauê Gabriel Rodrigues de Morais" << endl;
+
+    return 0;
+}
---
+++ Atividades/Caue_Gabriel/lista 1/L1EX10_Caue.cpp
@@ -0,0 +1,22 @@
+/**********************************************************
+- Autor:     Lineu Lima
+- Descrição: Comando cout
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "\tCauê\n" << endl;
+    cout << "\tGabriel" << endl;
+
+    return 0;
+}
