cca384ac92e859a21ac405437637785157a2089f
martinsleticia <53452764+martinsleticia@users.noreply.github.com>
2019-08-05 19:46:55 -0300

    Add files via upload

---
+++ Atividades/Letícia_Martins/Lista 1/L1EX09.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:     Lineu Lima
+- Descrição: Comando cout
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Letícia da Silva Martins!";
+
+    return 0;
+}
