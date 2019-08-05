67a7436ed02326dd06ba24593163891e78d1a34d
BrunoLepol <53452766+BrunoLepol@users.noreply.github.com>
2019-08-05 19:36:56 -0300

    Add files via upload

---
+++ Atividades/Bruno Henrique Leopoldino/L1EX05.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:        Bruno Henrique
+- Descrição: Comando \n - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Aula de Automação!" << endl;
+
+    return 0;
+}
