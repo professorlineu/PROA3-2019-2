b3623723082b0840c3c1989559d6fee35683f37c
jaderoliv <53538203+jaderoliv@users.noreply.github.com>
2019-08-05 19:49:45 -0300

    Add files via upload

---
+++ Atividades/Jader_Oliveira/lista1/EX01_Jader_Oliveira.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:     Jader Oliveira
+- Descrição: Comando cout
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Aula de Automação";
+
+    return 0;
+}
