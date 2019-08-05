3195ef9df42dda48e3b2d7d3c08ba240a3dacb90
adalbertojose <53452767+adalbertojose@users.noreply.github.com>
2019-08-05 20:43:34 -0300

    Add files via upload

---
+++ Atividades/adalberto_jose/lista1/L1EX06.cpp
@@ -0,0 +1,24 @@
+/**********************************************************
+- Autor:     Lineu Lima
+- Descrição: Comando \t - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "10 20 30" << endl;
+    cout << "40 50 60" << endl;
+    cout << "70 80 90" << endl;
+
+
+    return 0;
+
+}
