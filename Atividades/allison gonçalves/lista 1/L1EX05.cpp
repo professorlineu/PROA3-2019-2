641e3e6665e9c147a15c7951bc2b1d199e4ae1f8
isaaccarvalhoalmeida <53798369+isaaccarvalhoalmeida@users.noreply.github.com>
2019-08-05 20:31:43 -0300

    Add files via upload

---
+++ Atividades/allison gonçalves/lista 1/L1EX05.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+- Autor:     Lineu Lima
+- Descrição: Comando \t - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Aula de Automação!" << endl;
+
+
+    return 0;
+}
