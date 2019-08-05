189f2984e2009ca6d6ca08c47842eb91ae02f404
NeritonCavalheiro <53452913+NeritonCavalheiro@users.noreply.github.com>
2019-08-05 19:55:47 -0300

    Add files via upload

---
+++ Atividades/Nériton/lista1/arvore px.cpp
@@ -0,0 +1,34 @@
+/**********************************************************
+- Autor:Nériton R.S Cavalheiro
+- Descrição: Arvore
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    cout<<"      *"<<endl;
+    cout<<"     * *"<<endl;
+    cout<<"    *   *"<<endl;
+    cout<<"   *     *"<<endl;
+    cout<<"  *       *"<<endl;
+    cout<<" *         *"<<endl;
+    cout<<"*************"<<endl;
+    cout<<"     ***      "<<endl;
+    cout<<"     ***      "<<endl;
+    cout<<"     ***      "<<endl;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+
+
+    return 0;
+}
