cbda6c7acc7f7745d72e4a6849cb9f57798c73b9
Rbsgt <53452784+Rbsgt@users.noreply.github.com>
2019-08-05 20:42:26 -0300

    Add files via upload

---
+++ Atividades/Robson_Torres/lista 1/L1EX08.cpp
@@ -0,0 +1,29 @@
+/**********************************************************
+- Autor:     Robson Torres
+- Descrição: Comando \t - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "   * " << endl;
+    cout << "  *   *" <<  endl;
+    cout <<" *      *" << endl;
+cout << "  *         *" << endl;
+cout << "*            *" << endl;
+cout <<"*              *" << endl;
+cout <<" *************" << endl;
+  cout << "  *** " << endl;
+  cout << "  *** " << endl;
+  cout << "  *** " << endl;
+
+    return 0;
+}
