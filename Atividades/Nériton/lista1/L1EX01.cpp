6fa440aae6c06b2da0ec1cadb8021c89d0c45674
NeritonCavalheiro <53452913+NeritonCavalheiro@users.noreply.github.com>
2019-08-05 19:33:56 -0300

    Add files via upload

---
+++ Atividades/Nériton/lista1/L1EX01.cpp
@@ -0,0 +1,20 @@
+/**********************************************************
+- Autor:    Nériton R.S.Cavalheiro
+- Descrição: Atividade 1
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Aula de Automção";
+
+    return 0;
+}
