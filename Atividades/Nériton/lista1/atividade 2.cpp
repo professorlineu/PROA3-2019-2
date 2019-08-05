3084a823f379b6366d43fd600e99c715bde29e1e
NeritonCavalheiro <53452913+NeritonCavalheiro@users.noreply.github.com>
2019-08-05 19:36:38 -0300

    Add files via upload

---
+++ Atividades/Nériton/lista1/atividade 2.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+- Autor:     Nériton R.S Cavalheiro.
+- Descrição: exercio 7
+*********************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+//Declaração de variáveis
+
+
+//Configuração da tela de saída
+setlocale(LC_ALL,"");
+system("color F1");
+
+//Código do programa
+cout<< "10\t20"<<endl;
+cout<< "30\t40"<<endl;
+cout<< "50\t60"<<endl;
+
+
+
+return 0;
+}
