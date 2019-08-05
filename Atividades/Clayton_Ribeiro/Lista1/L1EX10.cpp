7e569fd543661a19ef0279947db72fe96802ab82
claytoncribeiro <53452732+claytoncribeiro@users.noreply.github.com>
2019-08-05 11:55:04 -0300

    Add files via upload

---
+++ Atividades/Clayton_Ribeiro/Lista1/L1EX10.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+- Autor:     Clayton C Ribeiro
+- Descrição: Comando \t - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "Clayton\nCarneiro Ribeiro" << endl;
+
+    return 0;
+}
