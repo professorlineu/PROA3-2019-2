0f527fac5992aae1ddc3dc906eb798a29e3e93ff
claytoncribeiro <53452732+claytoncribeiro@users.noreply.github.com>
2019-08-12 19:56:32 -0300

    Add files via upload

---
+++ Atividades/Clayton_Ribeiro/Lista2/L2EX12.cpp
@@ -0,0 +1,30 @@
+/**********************************************************
+- Autor:     Clayton C Ribeiro
+- Descrição: Lista 2 - Exercício 12
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    float  f_vlr_sal_MIN = 0, f_vlr_sal_FUNC = 0, f_num_sal = 0;
+
+    system("cls");
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    cout << "\nDigite o valor do seu salário: "; // Recebe valor do salário do funcionário
+    cin >> f_vlr_sal_FUNC;
+
+    cout << "\nDigite o valor do salário mínimo: "; // Recebe valor do salário mínimo
+    cin >> f_vlr_sal_MIN;
+
+    f_num_sal = f_vlr_sal_FUNC / f_vlr_sal_MIN; //Calcula a quantidade de salários mínimos
+
+    cout << "\nQuantidade de salários mínimos que você recebe: " << f_num_sal;
+
+	return 0;
+}
