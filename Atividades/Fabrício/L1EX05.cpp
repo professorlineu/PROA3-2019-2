843d494bf8b1d8c747ad80a65820981386664851
NunesdeAlvarenga <53799230+NunesdeAlvarenga@users.noreply.github.com>
2019-08-12 19:50:00 -0300

    Atividades Lista 1

---
+++ Atividades/Fabrício/L1EX05.cpp
@@ -0,0 +1,22 @@
+/**************************************
+- Autor: Fabrício Nunes
+- Descrição: Comando endl - Nova Linha
+**************************************/
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color A2");
+
+    //código do programa
+    cout << "Aula de Automação!" << endl;
+
+    return 0;
+
+}
