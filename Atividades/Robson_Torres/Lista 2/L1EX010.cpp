08135eb41b9587dc15bc280fe3e22d35997cac98
Rbsgt <53452784+Rbsgt@users.noreply.github.com>
2019-08-12 18:56:32 -0300

    lista 2

---
+++ Atividades/Robson_Torres/Lista 2/L1EX010.cpp
@@ -0,0 +1,21 @@
+/**********************************************************
+- Autor:     Robson Torres
+- Descrição: Comando \t - nova linha
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Robson  " << endl;
+
+    cout << "Gomes Torres " << endl;
+    return 0;
+}
