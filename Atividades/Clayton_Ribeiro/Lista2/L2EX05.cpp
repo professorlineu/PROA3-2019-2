abf4e4b736a7aef5987964d16da9ece18a607ad2
claytoncribeiro <53452732+claytoncribeiro@users.noreply.github.com>
2019-08-12 20:12:10 -0300

    Add files via upload

---
+++ Atividades/Clayton_Ribeiro/Lista2/L2EX05.cpp
@@ -0,0 +1,27 @@
+/**********************************************************
+- Autor:     Clayton C Ribeiro
+- Descrição: Lista 2 - Exercício 5
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    double  d_preco = 0, d_precoDesc = 0;
+
+    system("cls");
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    cout << "Digite o preço do produto: "; // Recebe valor do salário do funcionário
+    cin >> d_preco;
+
+    d_precoDesc = d_preco * ( 1 - 0.1 ); //Calcula o preço com 10% de desconto
+
+    cout << "\nO preço com desconto de 10% é: " << d_precoDesc;
+
+	return 0;
+}
