26763b90dfa282c228d6742e3682e01c6533cc55
adalbertojose <53452767+adalbertojose@users.noreply.github.com>
2019-08-13 09:02:27 -0300

    Add files via upload

---
+++ Atividades/adalberto_jose/lista2/lista2 exe 02.cpp
@@ -0,0 +1,40 @@
+/**********************************************************
+- Autor:    Adalberto José
+- Descrição: Lista 2 Exercicio 2
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    int iNumero1 = 0;
+    int iNumero2 = 0;
+    int iNumero3 = 0;
+    int iResultado = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Digite o numero1: ";
+    cin >> iNumero1;
+
+    cout << "Digite o numero3: ";
+    cin >> iNumero2;
+
+    cout << " Digite o numero3: ";
+    cin >>  iNumero3;
+
+    iResultado = iNumero1 * iNumero2 *iNumero3;
+
+    cout << "O numero é: " << iResultado;
+
+
+    return 0;
+}
