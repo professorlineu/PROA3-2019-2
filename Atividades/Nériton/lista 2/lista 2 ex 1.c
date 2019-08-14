f904be91dafd8391a07a877f8ba89306240fa718
NeritonCavalheiro <53452913+NeritonCavalheiro@users.noreply.github.com>
2019-08-14 20:08:39 -0300

    Add files via upload

---
+++ Atividades/Nériton/lista 2/lista 2 ex 1.c
@@ -0,0 +1,31 @@
+/**********************************************************
+- Autor:     Nériton
+- Descrição: lista 2 exer 1
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    int iano atual = 0;
+    int iano nasc = 0;
+    int resultado =0,
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+    cout  << " digite o ano atual ";
+    int >> iano atual;
+
+    iresultado = ianoatual - anode nasc;
+
+    //Código do programa
+
+
+
+    return 0;
+}
