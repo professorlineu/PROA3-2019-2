861fcad32dcc4efe7711b3cdd5da5fc252c6cd28
claytoncribeiro <53452732+claytoncribeiro@users.noreply.github.com>
2019-08-15 13:07:09 -0300

    Add files via upload

---
+++ Atividades/Clayton_Ribeiro/Lista3/L3EX01.cpp
@@ -0,0 +1,46 @@
+/**********************************************************
+- Autor:     Clayton C Ribeiro
+- Descrição: Lista 3 - Exercício 1
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    float  fNota01 = 0, fNota02 = 0, fNota03 = 0, fNota04 = 0;
+    float fMed = 0;
+
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    // Recebe o valor das notas
+
+    cout << "Digite a sua nota da P1: ";
+    cin >> fNota01;
+
+    cout << "Digite a sua nota da P2: ";
+    cin >> fNota02;
+
+    cout << "Digite a sua nota da P3: ";
+    cin >> fNota03;
+
+    cout << "Digite a sua nota da P4: ";
+    cin >> fNota04;
+
+    fMed = (fNota01 + fNota02 + fNota03 + fNota04) / 4; //Calcula o valor da média do aluno
+
+    if (fMed >= 7)
+    {
+        cout << "\nVoce foi APROVADO com média: " << fMed;
+    }
+
+    else
+    {
+        cout << "\nVoce foi REPROVADO com média: " << fMed;
+    }
+
+	return 0;
+}
