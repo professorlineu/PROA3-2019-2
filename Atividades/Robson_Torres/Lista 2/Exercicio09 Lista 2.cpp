79643d731702607f14c303fc4aeee671643a58a0
Rbsgt <53452784+Rbsgt@users.noreply.github.com>
2019-08-18 20:53:09 -0300

    Add files via upload

---
+++ Atividades/Robson_Torres/Lista 2/Exercicio09 Lista 2.cpp
@@ -0,0 +1,40 @@
+/**********************************************************
+- Autor:     Robson Torres
+- Descrição: Exercicio 09 Lista 2
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    float fBaseMaior = 0;
+    float fBaseMenor = 0;
+    float fAltura = 0;
+    float fArea = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color B1");
+
+    //Código do programa
+
+    cout << "Digite o valor da base maior do trapézio:  ";
+    cin >> fBaseMaior;
+
+    cout << "Digite o valor da base menor:  ";
+    cin >> fBaseMenor;
+
+    cout << "Digite o valor da altura:  ";
+    cin >> fAltura;
+
+    fArea = ((fBaseMaior + fBaseMenor) * fAltura)/2;
+
+    cout << "A Aréa do trapézio é igual à:  " << fArea << endl;
+
+    return 0;
+}
