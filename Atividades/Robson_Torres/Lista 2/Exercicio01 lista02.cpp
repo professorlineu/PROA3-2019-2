77ca2c1721a770108dd39b6dc73edde68b16b8c4
Rbsgt <53452784+Rbsgt@users.noreply.github.com>
2019-08-18 16:47:34 -0300

    Add files via upload

---
+++ Atividades/Robson_Torres/Lista 2/Exercicio01 lista02.cpp
@@ -0,0 +1,37 @@
+/**********************************************************
+- Autor:     Robson Torres
+- Descrição: Exercicio 01 Lista 2
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    double d_num1 = 0, d_num2 = 0, d_resultado = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "Digite o primeiro número: ";    // Recebe o valor da idade
+    cin >> d_num1;
+
+    cout << "Digite o segundo número: ";
+    cin  >> d_num2;
+
+
+    d_resultado = d_num1 - d_num2;
+
+    cout << "O resultado da subtração é: " << d_resultado;
+
+    return 0;
+}
+
