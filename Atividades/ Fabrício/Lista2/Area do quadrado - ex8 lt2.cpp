688f6bdf3d337b537876f8a499a3892152981313
NunesdeAlvarenga <53799230+NunesdeAlvarenga@users.noreply.github.com>
2019-08-26 19:59:20 -0300

    Add files via upload

---
+++ Atividades/ Fabrício/Lista2/Area do quadrado - ex8 lt2.cpp
@@ -0,0 +1,40 @@
+/**********************************************************
+- Autor: Fabricio Nunes
+- Descrição: Lista 2 - Exercício 8 - Área do quadrado em m²
+**********************************************************/
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+   float fLado1 = 0;
+   float fLado2 = 0;
+   float fArea  = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Digite o primeiro lado: ";
+    cin >> fLado1;
+
+    cout << "Digite o segundo lado: ";
+    cin >> fLado2;
+
+    cout << endl;
+
+    fArea = fLado1 * fLado2;
+
+    cout << "Área do trapézio = " << fArea << "m²" << endl;
+
+    return 0;
+}
