7f268974761768079f5160d0965a31822df36ac6
Cauebiel18 <53452755+Cauebiel18@users.noreply.github.com>
2019-08-28 20:42:09 -0300

    Add files via upload

---
+++ Atividades/Caue_Gabriel/lista 4/CAUE-LISTA-4-EX.4.cpp
@@ -0,0 +1,39 @@
+/**********************************************************
+- Autor:     CAUE GABRIEL
+- Descrição: LISTA 4 - EX.
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    int iValor = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F45");
+
+    //Código do programa
+
+    cout << "Digite o número da tabuada desejada: ";
+    cin >> iValor;
+
+    while (i < 10)
+    {
+        i = i + 1;
+
+        cout << iValor << " X " << i << " = " << iValor * i << endl;
+    }
+
+
+
+
+    return 0;
+}
---
+++ Atividades/Caue_Gabriel/lista 4/CAUE-LISTA-4-EX.5.cpp
@@ -0,0 +1,45 @@
+/**********************************************************
+- Autor:     CAUE GABRIEL
+- Descrição: LISTA 4 - EX.05
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    int j = 0;
+    int iResult = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    while (i <= 10)
+    {
+        cout << "\nTabuada do " << i << " : \n" << endl;
+
+        while (j <= 10)
+        {
+            iResult = j * i;
+
+            cout << j << " X " << i << " = " << iResult << endl;
+
+            j++;
+        }
+
+        i++;
+        j = 0;
+    }
+
+
+    return 0;
+}
