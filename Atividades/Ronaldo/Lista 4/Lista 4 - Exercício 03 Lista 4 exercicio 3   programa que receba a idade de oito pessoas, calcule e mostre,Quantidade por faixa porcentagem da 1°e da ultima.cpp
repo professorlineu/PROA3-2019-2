8b7e006afc29485bec9816ba40765b5afb4946cc
Ronaldo Santos Rodrigues <53453106+rrodri15@users.noreply.github.com>
2019-09-02 20:38:24 -0300

    Lista 4
    Llista 4 exercicio 3, 6, 7

---
+++ Atividades/Ronaldo/Lista 4/Lista 4 - Exercício 03 Lista 4 exercicio 3   programa que receba a idade de oito pessoas, calcule e mostre,Quantidade por faixa porcentagem da 1°e da ultima.cpp
@@ -0,0 +1,71 @@
+/**********************************************************
+- Autor:     Ronaldo Rodrigues
+- Descrição: Lista 4 - Exercício 03 Lista 4 exercicio 3   programa que receba a idade de oito pessoas, calcule e mostre,Quantidade por faixa porcentagem da 1°e da ultima
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    int iIdade = 0, i = 0;
+    int iCont01 = 0, iCont02 = 0, iCont03 = 0, iCont04 = 0, iCont05 = 0;
+    float fCont01, fCont05;
+    float fPorcent1 = 0, fPorcent5 = 0;
+
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    while (i < 8)
+    {
+        cout << "Digite a idade em anos: ";    // Recebe o valor da idade
+        cin >> iIdade;
+
+        i++;
+
+        if (iIdade <= 15)
+        {
+            iCont01++;
+        }
+
+        else if (iIdade >= 16 && iIdade <= 30)
+        {
+            iCont02++;
+        }
+
+        else if (iIdade >= 31 && iIdade <= 45)
+        {
+            iCont03++;
+        }
+
+        else if (iIdade >= 46 && iIdade <= 60)
+        {
+            iCont04++;
+        }
+
+        else
+        {
+            iCont05++;
+        }
+
+    } // Fim do while
+
+    cout << "FAIXA ETÁRIA \tIDADE \t\t\t QUANTIDADE" << endl;
+    cout << "1 \t\tAté 15 anos \t\t " << iCont01 << endl;
+    cout << "2 \t\tDe 16 a 30 anos \t " << iCont02 << endl;
+    cout << "3 \t\tDe 31 a 45 anos \t " << iCont03 << endl;
+    cout << "4 \t\tDe 46 a 60 anos \t " << iCont04 << endl;
+    cout << "5 \t\tAcima de 60 anos \t " << iCont05 << endl;
+
+    fCont01 = iCont01;
+    fPorcent1 = (fCont01/i) * 100;
+    cout << "Porcentagem de pessoas na primeira faixa etária: " << fPorcent1 << "%" << endl;
+
+    fCont05 = iCont05;
+    fPorcent5 = (fCont05/i) * 100;
+    cout << "Porcentagem de pessoas na última faixa etária: " << fPorcent5 << "%" << endl;
+
+	return 0;
+}
---
+++ Atividades/Ronaldo/Lista 4/Lista 4 exercicio 7 programa que receba a idade, a altura e o peso de cinco pessoas, calcule e mostre.cpp
@@ -0,0 +1,65 @@
+/**********************************************************
+- Autor:     Ronaldo Rodrigues
+- Descrição: Lista 4 - Exercício 07 Lista 4 exercicio 7 programa que receba a idade, a altura e o peso de cinco pessoas, calcule e mostre - 01 a quantidade de pessoas com idade superior a 50 anos -02 a média das alturas das pessoas com idade entre 10 e 20 anos - 03 a porcentagem de pessoas com peso inferior a 40 kg entre todas as pessoas analisadas
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main ()
+{
+    int i = 0;
+    int iIdade = 0;
+    float fAltura = 0, fPeso = 0;
+    float fAlturaTotal02 = 0;
+    float fAlturaMed02 = 0;
+    int iCont01 = 0, iCont02 = 0;
+    float fCont03 = 0;
+    float fPorcent03 = 0;
+
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    while (i < 5)
+    {
+        cout << "\nDigite a idade em anos: ";    // Recebe o valor da idade
+        cin >> iIdade;
+
+        cout << "Digite a altura em metros: ";    // Recebe o valor da altura
+        cin >> fAltura;
+
+        cout << "Digite o peso em quilogramas: ";    // Recebe o valor do peso
+        cin >> fPeso;
+
+        i++;
+
+        if (iIdade > 50)
+        {
+            iCont01++;
+        }
+
+        if (iIdade >= 10 && iIdade <= 20)
+        {
+            iCont02++;
+            fAlturaTotal02 += fAltura;
+        }
+
+        if (fPeso < 40)
+        {
+            fCont03++;
+        }
+
+    } // Fim do while
+
+    cout << "A quantidade de pessoas com idade superior a 50 anos: " << iCont01 << endl;
+
+    fAlturaMed02 = fAlturaTotal02/iCont02;
+    cout << "A média das alturas das pessoas com idade entre 10 e 20 anos: " << fAlturaMed02 << endl;
+
+    fPorcent03 = (fCont03/i) * 100;
+    cout << "A porcentagem de pessoas com peso inferior a 40 kg entre todas as pessoas analisadas: " << fPorcent03 << "%" << endl;
+
+	return 0;
+}
