3df137193d9ed20eb821e82473707a52688b83a2
Bruno Henrique Leopoldino da Silva Costa <53452766+BrunoLepol@users.noreply.github.com>
2019-09-09 20:30:39 -0300

    Add files via upload

---
+++ Atividades/Bruno Henrique Leopoldino/Lista 4/EX 7.cpp
@@ -0,0 +1,90 @@
+/**********************************************************
+- Autor:     Bruno Henrique
+- Descrição: EX7
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    int iIdade = 0;
+    float fAltura =0;
+    float fPeso =0;
+    int iPessoas1 =0;
+    int iPessoas2 =0;
+    int iPessoas3 =0;
+    int iPorcen =0;
+    float fSoma=0;
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    while (i < 2)
+    {
+        cout << "\nIndique a Idade: ";
+        cin >> iIdade;
+
+        cout  << "\nIndique a Altura: ";
+        cin >> fAltura;
+
+        cout << "\nIndique o peso: ";
+        cin >> fPeso;
+
+        i++;
+
+        if (iIdade > 50)
+        {
+            iPessoas1++;
+        }
+
+        else if (iIdade >= 10 && iIdade <= 20)
+        {
+             fSoma = fSoma + fAltura;
+       iPessoas2++;
+
+        }
+          if (fPeso < 40)
+   {
+     iPessoas3++;
+   }
+    }
+    if (iPessoas1 > 0 )
+    {
+        cout << "\nPessoas com mais de 50:" << iPessoas1;
+    }
+
+    else
+    {
+        cout << "\nNão há pessoas com mais de 50 anos!";
+    }
+
+    if (iPessoas2 > 0)
+    {
+       cout << "\nA média de pessoas com idade entre 10 a 20 anos é de:"  << fSoma/iPessoas2;
+    }
+
+
+    iPorcen = (((float)iPessoas3/i)*100);
+
+
+    if (iPessoas3 > 0)
+    {
+        cout <<
+         "\nA porcentagem de pessoas com peso inferior a 40kg é de" << iPorcen << "%";
+    }
+
+    else
+    {
+        cout << "\nNenhum dos pesos digitados foi inferior a 40kg";
+    }
+
+     return 0;
+}
