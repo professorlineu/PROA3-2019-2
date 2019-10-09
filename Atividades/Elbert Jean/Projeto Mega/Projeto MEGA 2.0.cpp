d660f88c6e545da05a16047ef13f2a615bcf23c4
ElbertJean <53452699+ElbertJean@users.noreply.github.com>
2019-10-09 20:41:46 -0300

    Add files via upload

---
+++ Atividades/Elbert Jean/Projeto Mega/Projeto MEGA 2.0.cpp
@@ -0,0 +1,162 @@
+/**********************************************************
+- Autor:     Adriel Silveira / Elbert Jean
+- Descrição: Jogo da MEGA - v 1.0
+**********************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <locale.h>
+#include <time.h> // necessário para gerar número aleatório
+#include <windows.h>
+using namespace std;
+
+void SetColor(int ForgC);
+
+int main()
+{
+    int iAleatorio = 0; // número gerado pelo computador
+    int iNumero = 0; // valor do usuário
+    int iNivel = 0; // valor do usuário
+    int i= 0;
+    int icont = 0;
+    int vet[6] ;
+    int vet2[6] ;
+
+
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+
+    // evita que o número sorteado seja sempre o mesmo a cada execução
+    srand(time(NULL));
+
+    cout << " \n     JOGO DA MEGA ALEATÓRIO " << "\n";
+    cout <<"\n       TABELA DE PONTUAÇÃO" << endl ;
+    cout <<"\nTENTATIVAS \t\t PONTOS";
+    cout <<"\n    1 \t\t\t   10";
+    cout <<"\n  2 a 3 \t\t    7";
+    cout <<"\n  4 a 5 \t\t    5";
+    cout <<"\n  6 a 7 \t\t    3";
+    cout <<"\n  8 a 9 \t\t    2";
+    cout <<"\n   10\t\t\t    1";
+    cout << endl ;
+
+
+
+
+    while ( i<6 )
+
+    {
+        iAleatorio = rand() % 6+ 1;
+
+         icont = 0;
+
+
+
+        do
+
+        {
+            icont++ ;
+
+
+            SetColor(4);
+            cout << "\nDigite um número de 1 a 6: ";
+            cin >> iNumero;
+
+            if (iAleatorio > iNumero)
+            {
+                SetColor(6);
+                cout << "Pense em um número MAIOR!" << endl;
+                SetColor(5);
+            }
+            else if (iAleatorio < iNumero)
+            {
+                SetColor(6);
+                cout << "Pense em um número MENOR!" << endl;
+                SetColor(5);
+            }
+
+             // fim do else
+            else
+            {
+                cout << "\nParabéns, você acertou na " ;
+
+            }
+
+
+
+
+             cout << "Tentativa Número " <<icont << endl ;
+
+
+
+
+        }
+
+
+        while (iAleatorio != iNumero);
+
+         SetColor(1);
+        cout << "\nO número aleatório sorteado nessa rodada foi o " << iAleatorio <<endl << endl ;
+
+        vet[i] = iAleatorio ;
+
+        vet2[i] = icont ;
+
+
+
+        i++;
+    } // fim do while repetidor
+
+    cout << "\nOs numeros sorteados nesse jogo foram :" <<endl;
+    for(i=0;i<6;i++)
+    {
+        cout << vet[i] << "\t " ;
+    }
+
+    cout << "\n" ;
+
+    cout << "\nQuantidade de tentaivas para cada acerto foram:" <<endl;
+    for(i=0;i<6;i++)
+    {
+        cout << vet2[i] << "\t " ;
+
+
+    if (vet2[i]  == 1)
+    {
+    cout <<"10 pontos";
+    }
+
+
+
+
+
+    else
+    {
+        cout <<"NENHUM PONTO";
+    }
+
+    }
+
+} // fim do main
+
+
+
+
+
+
+void SetColor(int ForgC)
+{
+    WORD wColor;
+    //We will need this handle to get the current background attribute
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    //We use csbi for the wAttributes word.
+    if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+    {
+        //Mask out all but the background attribute, and add in the forgournd color
+        wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+        SetConsoleTextAttribute(hStdOut, wColor);
+    }
+    return;
+}
